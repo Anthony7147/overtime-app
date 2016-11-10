28e8dc24241b58be7e981eaa24508beead8c7681
Anthony <Anthony>
2016-11-10 16:41:25 -0700

    Added update callback for audit logs to set confirmed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 
 puts "1 employee created"
 
-AdminUser.create(email: "anthonyknoll@yahoo.com",
+AdminUser.create(email: "admin@test.com",
                   password: "asdfasdf",
                   password_confirmation: "asdfasdf",
                   first_name: "Admin",
